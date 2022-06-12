4314ac5b7fd487a36577cc5b7fe342ee4d30dcb2
ShayDPanda <shaydepanda@gmail.com>
2022-06-12 14:08:33 -0700

    Marketplace SQL connected

--- RTXWebsite1/Models/Marketplace.cs
+++ RTXWebsite1/Models/Marketplace.cs
@@ -4,7 +4,10 @@ namespace RTXWebsite1.Models
 {
 	public class Marketplace
 	{
-
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Cost { get; set; }
 	
 	}
 }
