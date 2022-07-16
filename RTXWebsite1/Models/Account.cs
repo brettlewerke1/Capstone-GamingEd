86730f12728b0c0bb2621d1bf64f138b4b1fcbae
ShayDPanda <shaydepanda@gmail.com>
2022-07-16 14:58:09 -0700

    Phase One of SQL Overhaul. Can correctly display multiple classes in account.

--- RTXWebsite1/Models/Account.cs
+++ RTXWebsite1/Models/Account.cs
@@ -4,6 +4,12 @@ namespace RTXWebsite1.Models
 {
     public class Account
     {
+        public int Account_ID { get; set; }
+
+        public string Account_Username { get; set; }
+
+        public string Account_Password { get; set; }
+        
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
@@ -13,6 +19,7 @@ namespace RTXWebsite1.Models
 
         [Required]
         public string Password { get; set; }
+        
         public string CurrentClass { get; set; }
         public int Coins { get; set; }
 
@@ -21,5 +28,17 @@ namespace RTXWebsite1.Models
         public string ClassTwo { get; set; }
 
         public string Role { get; set; }
+    }
+
+    public class PlayerInformation
+    {
+        public int Account_ID { get; set; }
+        public string Account_Username { get; set; }
+        public string Account_Password { get; set; }
+        public int Player_Coins { get; set; }
+        public string Player_IGN { get; set; }
+        public int Course_ID { get; set; }
+        public string Course_Name { get; set; } 
+        public string Course_Tag { get; set; }
     }
 }
