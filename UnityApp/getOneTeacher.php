980d5255f2c315a43d8f8a4ec1b3fc45a6cb4e14
chasespigarelli <78243585+chasespigarelli@users.noreply.github.com>
2022-09-11 19:19:30 -0700

    Connecting getOneTeacher.php to RTX Database

--- UnityApp/getOneTeacher.php
+++ UnityApp/getOneTeacher.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'account');
+    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'RTX');
 
     if(mysqli_connect_error())
     {
@@ -7,18 +7,23 @@
         exit();
     }
 
+
     $username = $_POST["Username"];
 
-
-    $queryString = "SELECT * FROM account.people WHERE Username='".$username."';";
+    // SQL query
+    $queryString = "SELECT * FROM RTX.Account WHERE Account_Username='".$username."';";
 
     $teachers = mysqli_query($con, $queryString) or die("2:...DB issue"); // error code 2 = name already in Db
     $userInfo = mysqli_fetch_assoc($teachers);
+
+    // fetching variables from database
     $returnedFirstname = $userInfo["Firstname"];
     $returnedLastname = $userInfo["Lastname"];
-    $returnedUsername = $userInfo["Username"];
+    $returnedUsername = $userInfo["Account_Username"];
     $returnedRole = $userInfo["Role"];
     $returnedClass = $userInfo["CurrentClass"];
+
+    // printing variables receieved from database
     echo($returnedRole." ".$returnedUsername." ".$returnedFirstname." ".$returnedLastname." ".$returnedClass);
 
 ?>
