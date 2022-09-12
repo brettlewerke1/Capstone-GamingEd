8eeda67496af0f8dad08d68faac41415b24b86cd
chasespigarelli <78243585+chasespigarelli@users.noreply.github.com>
2022-09-11 19:18:31 -0700

    Connecting getOneCourse.php to RTX Database

--- UnityApp/getOneCourse.php
+++ UnityApp/getOneCourse.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'account');
+    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'RTX');
 
     
     if(mysqli_connect_error())
@@ -10,7 +10,9 @@
 
     $classNum = $_POST["ClassNumber"];
 
-    $queryString = "SELECT * FROM account.students WHERE StudentClass='".$classNum."';";
+    $student = "student";
+
+    $queryString = "SELECT * FROM RTX.Account WHERE CurrentClass='".$classNum."' AND Role='".$student."';";
 
     $students = mysqli_query($con, $queryString) or die("2:...DB issue"); // error code 2 = name already in Db
     $rows = array();
@@ -22,7 +24,7 @@
     }
 
     foreach($rows as $row){
-        echo ($row["StudentName"] ."        ".$row["StudentUsername"]."        ".$row["StudentClass"]."/"); 
+        echo ($row["Firstname"] ."        ".$row["Account_Username"]."        ".$row["CurrentClass"]."/"); 
     }
 
 ?>
