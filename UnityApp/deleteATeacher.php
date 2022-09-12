833ce84fefed1b44a697389915b24976d270814a
chasespigarelli <78243585+chasespigarelli@users.noreply.github.com>
2022-09-11 19:15:41 -0700

    Connecting deleteATeacher.php to RTX Database

--- UnityApp/deleteATeacher.php
+++ UnityApp/deleteATeacher.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'account');
+    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'RTX');
 
 
     if(mysqli_connect_error())
@@ -11,14 +11,14 @@
     
     $username = $_POST["Username"];
 
-    $queryString = "SELECT * FROM account.people WHERE Username='".$username."';";
+    $queryString = "SELECT * FROM RTX.Account WHERE Account_Username='".$username."';";
 
     $teacher = mysqli_query($con, $queryString) or die("2:...DB issue"); // error code 2 = name already in Db
     $userInfo = mysqli_fetch_assoc($teacher);
 
-    $userId = $userInfo["Id"];
+    $userId = $userInfo["Account_ID"];
 
-    $queryString = "DELETE FROM account.people WHERE Id='".$userId."';";
+    $queryString = "DELETE FROM RTX.Account WHERE Account_ID='".$userId."';";
 
     $teacher = mysqli_query($con, $queryString) or die("2:...DB issue"); // error code 2 = name already in Db
     if($teacher == false)
