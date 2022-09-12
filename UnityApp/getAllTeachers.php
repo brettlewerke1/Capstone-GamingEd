9c931764b597d296b080ee8bf1b7f032716acd20
chasespigarelli <78243585+chasespigarelli@users.noreply.github.com>
2022-09-11 19:17:40 -0700

    Connecting getAllTeachers.php to RTX Database

--- UnityApp/getAllTeachers.php
+++ UnityApp/getAllTeachers.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'account');
+    $con = mysqli_connect('gaminged-db.czq2j2udebs7.us-west-2.rds.amazonaws.com', 'superAdmin', 'ghahyat8', 'RTX');
 
     if(mysqli_connect_error())
     {
@@ -9,7 +9,7 @@
 
 
     // get all teachers in the database
-    $queryString = "SELECT * FROM account.people WHERE Role='admin';";
+    $queryString = "SELECT * FROM RTX.Account WHERE Role='admin';";
 
 
     $teachers = mysqli_query($con, $queryString) or die("2:...DB issue"); // error code 2 = name already in Db
@@ -22,6 +22,6 @@
     }
 
     foreach($rows as $row){
-        echo ($row["Username"] ." "); 
+        echo ($row["Account_Username"] ." "); 
     }
 ?>
