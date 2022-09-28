4f56f65aa72a32a11903100dc112ca84ee587c11
chasespigarelli21 <78243585+chasespigarelli@users.noreply.github.com>
2022-09-27 22:22:07 -0700

    Matching in progress...

--- RTXWebsite1/Models/Quiz.cs
+++ RTXWebsite1/Models/Quiz.cs
@@ -5,41 +5,56 @@ namespace RTXWebsite1.Models
     public class Quiz
     {
 
+        
         public int Quiz_ID { get; set; }
-
-        public string Quiz_Word_Val { get; set; } = "";
         public int Quiz_Number { get; set; }
 
+
+
         public int Quiz_Question_Number { get; set; }
-
         public string Quiz_Question { get; set; } = "";
+        public string Question_Type { get; set; }
 
-        public string Quiz_Answers { get; set; } = "";
 
+        
+        // answers
         public string Quiz_Answer { get; set; } = "";
-
+        public string Quiz_Answers { get; set; } = "";
         public string Quiz_Answer_A { get; set; }
-
         public string Quiz_Answer_B { get; set; }
         public string Quiz_Answer_C { get; set; }
         public string Quiz_Answer_D { get; set; }
         public string Quiz_Answer_E { get; set; }
-
         public string Quiz_Answer_F { get; set; }
+
+
+
+
+
+        // entered answers
+        public string Entered_Answer_A { get; set; } = "";
+        public string Entered_Answer_B { get; set; } = "";
+        public string Entered_Answer_C { get; set; } = "";
+        public string Entered_Answer_D { get; set; } = "";
+        public string Entered_Answer_E { get; set; } = "";
+        public string Entered_Answer_F { get; set; } = "";
+
+
+
+
+
+        // correct answers
         public string Quiz_Correct_Answer { get; set; } = "";
         public string Quiz_Correct_Answer_A { get; set; } = "";
         public string Quiz_Correct_Answer_B { get; set; } = "";
 
 
-        // Quiz Results table
-        public int QuizResults_ID { get; set; }
 
-        public int QuizResults_AccountID { get; set; }
 
+
+        // Quiz results
         public int numCorrect { get; set; }
-
         public int numberOfQuestions { get; set; }
-
         public string Account_Role { get; set; }
 
     }
