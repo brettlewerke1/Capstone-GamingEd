b785ba3a19ed237dd583867b21a43e15214b2b94
ShayDPanda <shaydepanda@gmail.com>
2022-10-10 15:29:44 -0700

    Avatar Purchasing Functional

--- RTXWebsite1/Models/AssessmentAnswers.cs
+++ RTXWebsite1/Models/AssessmentAnswers.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace RTXWebsite1.Models
+﻿namespace RTXWebsite1.Models
 {
 	public class AssessmentAnswers
 	{
@@ -8,24 +6,20 @@ namespace RTXWebsite1.Models
 		public int AssessmentAnswer_ID { get; set; }
 
 		// different options for each question
-		public string AssessmentAnswer_Answers { get; set; }
+		public string? AssessmentAnswer_Answers { get; set; }
 
 
 		// foreign key
 		public int AssessmentAnswer_Assessment_ID { get; set; }
 
 		// matching answers for matching questions only
-		public string AssessmentAnswer_Matching_Answer { get; set; }
+		public string? AssessmentAnswer_Matching_Answer { get; set; }
 
 		// selected answer by the user
-		public string AssessmentAnswer_Selected_Answers {get; set; }
+		public string? AssessmentAnswer_Selected_Answers { get; set; }
 
 		// the correct answers to the question
-		public string AssessmentAnswer_Correct_Answers { get; set; }
-
-
-
-
+		public string? AssessmentAnswer_Correct_Answers { get; set; }
 
 	}
 }
