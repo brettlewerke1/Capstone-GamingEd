7cac7a18e134751ee3769910670d85a7c3e8d7a3
chasespigarelli21 <78243585+chasespigarelli@users.noreply.github.com>
2022-10-13 20:23:03 -0700

    4

--- RTXWebsite1/Models/Assignment.cs
+++ RTXWebsite1/Models/Assignment.cs
@@ -3,7 +3,7 @@
     public class Assignment
     {
 
-        public string submissionString { get; set; }
+        public string submissionString { get; set; } = "hellothere";
 
 
         public string Data { get; set; } = "somestring";
