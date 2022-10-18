fee522ff164279d080a37919648f56af29668517
chasespigarelli21 <78243585+chasespigarelli@users.noreply.github.com>
2022-10-17 23:13:14 -0700

    Display when the file is uploading

--- RTXWebsite1/Models/Assignment.cs
+++ RTXWebsite1/Models/Assignment.cs
@@ -4,7 +4,16 @@
     {
 
         // create getters and setters here
-        
+        public string? Assignment_Upload { get; set; }
+
+
+        public int Assignment_ID { get; set; }
+
+        public string Assignment_Title { get; set; }
+
+        public int Assignment_PlayerID { get; set; }
+
+
 
     }
 
