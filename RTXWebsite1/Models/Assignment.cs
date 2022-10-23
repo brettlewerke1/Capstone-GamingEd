8aae85eec33982f22d9bcd03d0b6747bbf4cce6a
chasespigarelli21 <78243585+chasespigarelli@users.noreply.github.com>
2022-10-23 16:45:52 -0700

    Ability to submit image, text, word, excel files

--- RTXWebsite1/Models/Assignment.cs
+++ RTXWebsite1/Models/Assignment.cs
@@ -9,6 +9,21 @@
 
 
 
+        public string Assignment_Type { get; set; }
+
+        public string PDF_File { get; set; } = "PDF";
+
+
+        public string Text_File { get; set; } = "Text";
+
+        public string Image_File { get; set; } = "Image";
+
+        public string Word_File { get; set; } = "Word";
+
+        public string Excel_File { get; set; } = "Excel";
+
+
+
 
     }
 
