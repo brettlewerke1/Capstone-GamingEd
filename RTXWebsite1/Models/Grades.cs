e2ece022cb47323c324126bd3742fe90936d87b1
chasespigarelli21 <78243585+chasespigarelli@users.noreply.github.com>
2022-10-23 19:09:32 -0700

    Started Gradebook: Displays grades for a specific user, module, and object

---
+++ RTXWebsite1/Models/Grades.cs
@@ -0,0 +1,20 @@
+﻿namespace RTXWebsite1.Models
+{
+    public class Grades
+    {
+        public int Grades_ModuleID { get; set; }
+
+
+
+        // score on specific assignment
+        public int Grades_Score { get; set; }
+
+        // what score was out of on a specific assignment
+        public int Grades_Total { get; set; }
+
+
+
+        public int Grades_ObjectID { get; set; }
+    }
+
+}
