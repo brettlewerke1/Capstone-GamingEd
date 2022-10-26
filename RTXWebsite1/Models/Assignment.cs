7d785d44204f9ff702b6af0c2875300a0302a3f4
ShayDPanda <shaydepanda@gmail.com>
2022-10-25 21:21:19 -0700

    Refactoring Assignment. Is not able to take from multiple users for the same assignment.

--- RTXWebsite1/Models/Assignment.cs
+++ RTXWebsite1/Models/Assignment.cs
@@ -2,28 +2,51 @@
 {
     public class Assignment
     {
+        public int Assignment_ID { get; set; }
+        public string? Assignment_Title { get; set; }
+        public string? Assignment_Desc { get; set; }
+        public string? Assignment_StartDate { get; set; }
+        public string? Assignment_EndDate { get; set; }
+        public int Assignment_TotalScore { get; set; }
 
-        
-        // base 64 assignment string
-        public string? Assignment_String_Upload { get; set; }
+        public static string GetSQL()
+        {
+            return @"
+            SELECT 
+                `Assignment`.`Assignment_ID` AS `Assignment_ID`,
+                `Assignment`.`Assignment_Title` AS `Assignment_Title`,
+                `Assignment`.`Assignment_Desc` AS `Assignment_Desc`,
+                `Assignment`.`Assignment_StartDate` AS `Assignment_StartDate`,
+                `Assignment`.`Assignment_EndDate` AS `Assignment_EndDate`,
+                `Assignment`.`Assignment_TotalScore` AS `Assignment_TotalScore`
+            FROM
+                `Assignment`";
+        }
+    }
 
+    public class AssignmentSubmissions
+    {
+        public int AsssignmentS_ID { get; set; }
+        public int AssignmentS_AssignmentID { get; set; }
+        public int AssignmentS_PlayerID { get; set; }
+        public string? AssignmentS_Comment { get; set; }
+        public string? AssignmentS_StringUpload { get; set; }
+        public string? AssignmentS_FileType { get; set; }
 
-        // assignment type
-        public string? Assignment_Type { get; set; }
-
-
-        // assignment types
-        public string PDF_File { get; set; } = "PDF";
-        public string Text_File { get; set; } = "Text";
-        public string Image_File { get; set; } = "Image";
-        public string Word_File { get; set; } = "Word";
-        public string Excel_File { get; set; } = "Excel";
-
-
-
-        // commants
-        public string? Assignment_Comment { get; set; }
-
+        public static string GetSQL()
+        {
+            return @"
+            SELECT 
+                `AssignmentSubmissions`.`AssignmentS_ID` AS `AssignmentS_ID`,
+                `AssignmentSubmissions`.`AssignmentS_AssignmentID` AS `AssignmentS_AssignmentID`,
+                `AssignmentSubmissions`.`AssignmentS_PlayerID` AS `AssignmentS_PlayerID`,
+                `AssignmentSubmissions`.`AssignmentS_Comment` AS `AssignmentS_Comment`,
+                `AssignmentSubmissions`.`Assignment_StringUpload` AS `Assignment_StringUpload`,
+                `AssignmentSubmissions`.`AssignmentS_FileType` AS `AssignmentS_FileType`
+            FROM
+                `AssignmentSubmissions`";
+            // WHERE AssignmentS_AssignmentID == var AND AssignmentS_PlayerID = var
+        }
     }
 
 }
